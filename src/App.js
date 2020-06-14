3c2170daf0a40806a0c06de8cc93f2ac271b42d9
David Alexander Leigh <daleighan@gmail.com>
2020-06-14 13:03:25 -0700

    Initial work done on a reducer for holding state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useReducer, useEffect} from 'react';
 import './App.scss';
 
+const initialState = {loaded: false};
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'TOGGLE_LOADED':
+      return {
+        ...state,
+        loaded: !state.loaded,
+      }
+    default:
+      return {...state}
+  }
+};
+
 function KlarityApp() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({type: 'TOGGLE_LOADED'})
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import KlarityApp from './KlarityApp';
+import KlarityApp from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
