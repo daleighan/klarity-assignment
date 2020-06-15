34d253396df2abe0daa5542e019dde4daf633ab4
David Alexander Leigh <daleighan@gmail.com>
2020-06-15 04:50:09 -0700

    Adding buttons to the sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -7,6 +7,18 @@ function Sidebar() {
       <div>
         <img src="/logo.png" className="logo" alt="logo" />
       </div>
+      <div className="nav-bar add-shadow">
+        <div className="nav-btns">
+          {[0, 1, 2, 3, 4, 5, 6].map(num => (
+            <img
+              key={num}
+              className="menu-item"
+              src="/menu.png"
+              alt="menu-item"
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
